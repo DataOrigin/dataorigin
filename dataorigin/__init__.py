808d1f2eff399e89468b09e752e9f16f0b551a62
Joaquin Montesinos <jkm280294@gmail.com>
2025-12-30 17:32:13 +0100

    1.6.3

--- dataorigin/__init__.py
+++ dataorigin/__init__.py
@@ -1,4 +1,13 @@
 from . import google_sheets
 
 __all__ = ["google_sheets"]
-__version__ = "1.6.1"
+try:
+    from importlib.metadata import PackageNotFoundError, version
+except Exception:  # noqa: BLE001
+    version = None
+    PackageNotFoundError = Exception
+
+try:
+    __version__ = version("dataorigin") if version else "0.0.0"
+except PackageNotFoundError:
+    __version__ = "0.0.0"
